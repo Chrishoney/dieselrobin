97f97b7c124a3dacf0e035ec2864d42a35be45b0
Chris Honey <chrishoney@roarlabs.com>
2012-12-25 08:38:25 -0600

    Add unicode methods for each db model.

--- dieselsite/diesel/models.py
+++ dieselsite/diesel/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+
 
 class Team(models.Model):
     name = models.CharField(max_length=50)
     score = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return u'<Team - %s>' % self.name
+
+    def players(self):
+        return self.players.filter(team=self.name)
+
 
 class Player(models.Model):
     name = models.ForeignKey(User)
@@ -13,19 +20,39 @@ class Player(models.Model):
     completed_missions = models.PositiveIntegerField(default=0)
     killed_characters = models.PositiveIntegerField(default=0)
 
+    def __unicode__(self):
+        return u'%s' % self.name.username
+
 class Character(models.Model):
+    # data
     combo = models.CharField(max_length=4)
     team = models.ForeignKey(Team, related_name='characters')
+    points = models.PositiveIntegerField(default=0)
+    # flags
     alive = models.BooleanField(default=True)
+    won = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return u'<Character - %s>' % self.combo
 
 class Mission(models.Model):
     player = models.ForeignKey(Player)
     combo = models.ForeignKey(Character)
 
     description = models.TextField()
+    new_locations = models.CharField(
+        max_length=255, help_text="New locations allowed by this mission"
+    )
     complete = models.BooleanField(default=False)
     failed = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return u'<Mission %s>' % self.id
 
     @property
     def number(self):
         return self.id
+
+    @property
+    def short_name(self):
+        return u'%s %s' % ('Mission', self.number)
