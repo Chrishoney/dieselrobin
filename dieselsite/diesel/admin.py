9d40d72bd865993d9afcf18c58c7c0faf357a682
Chris Honey <chrishoney@roarlabs.com>
2012-12-25 09:19:46 -0600

    Add a basic admin.py file for the diesel app.

---
+++ dieselsite/diesel/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from diesel.models import Team, Player, Character, Mission
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Character)
+admin.site.register(Mission)
