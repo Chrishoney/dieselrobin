20b1db47d86c5f8da8e5a4928072cb8c21d95529
Chris Honey <chrishoney@roarlabs.com>
2012-12-25 07:28:11 -0600

    Commit current progress to add project to github

--- dieselsite/diesel/models.py
+++ dieselsite/diesel/models.py
@@ -7,12 +7,25 @@ class Team(models.Model):
     score = models.PositiveIntegerField(default=0)
 
 class Player(models.Model):
-    name = models.CharField(max_length=20, unique=True)
+    name = models.ForeignKey(User)
     team = models.ForeignKey(Team, related_name='players')
+
+    completed_missions = models.PositiveIntegerField(default=0)
+    killed_characters = models.PositiveIntegerField(default=0)
 
 class Character(models.Model):
     combo = models.CharField(max_length=4)
     team = models.ForeignKey(Team, related_name='characters')
+    alive = models.BooleanField(default=True)
 
 class Mission(models.Model):
-    pass
+    player = models.ForeignKey(Player)
+    combo = models.ForeignKey(Character)
+
+    description = models.TextField()
+    complete = models.BooleanField(default=False)
+    failed = models.BooleanField(default=False)
+
+    @property
+    def number(self):
+        return self.id
