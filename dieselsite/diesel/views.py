a3d08758ba4d703c9c10f15c61f3ef46e9a1034d
Chris Honey <chrishoney@roarlabs.com>
2013-01-09 05:36:46 -0600

    Add basic view functions for select models.
    Six basic view functions for testing templates and models have been added to the
    diesel app. These seem repetitive, I should look into simplying the basic case.

--- dieselsite/diesel/views.py
+++ dieselsite/diesel/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+
+from diesel.models import Team, Player, Character
+
+def team(request, team, template='team.html'):
+    team = get_object_or_404(Team, name=team)
+    return render(request, template, {'team': team})
+
+def teams(request, template='teams.html'):
+    teams = Team.objects.all()
+    return render(request, template, {'teams': teams})
+
+def player(request, player, template='player.html'):
+    player = get_object_or_404(Player, name=player)
+    return render(request, template, {'player': player})
+
+def players(request, template='players.html'):
+    players = Player.objects.all()
+    return render(request, template, {'players': players})
+
+def character(request, character, template='characters.html'):
+    character = get_object_or_404(Character, combo)
+    return render(request, template, {'character': character})
+
+def characters(request, template="characters.html"):
+    characters = Character.objects.all()
+    return render(request, template, {'characters': characters})
