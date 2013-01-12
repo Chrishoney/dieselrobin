95572105505577e89b92f4e6a3bcfb2030312a71
Chris Honey <chrishoney@roarlabs.com>
2013-01-12 02:44:41 -0600

    Create new document for regular missions.

--- dieselsite/dieselsite/urls.py
+++ dieselsite/dieselsite/urls.py
@@ -6,4 +6,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+) += patterns('info.views',
+    # these are included here instead of in a separate file
+    url(r'^rules/$', 'rules', name='rules'),
+    url(r'^combos/$', 'combos', name='combos'),
+    url(r'^missions/bonus/$', 'bonus', name='bonus'),
+    url(r'^missions/regular/$', 'regular', name='regular'),
 )
