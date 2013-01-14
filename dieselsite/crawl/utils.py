29372fb513fc6317b557ea6b9623a84135af9c49
Chris Honey <chrishoney@roarlabs.com>
2013-01-14 02:27:31 -0600

    Finish valid combo algorithm. I think it's wrong.

--- dieselsite/crawl/utils.py
+++ dieselsite/crawl/utils.py
@@ -70,62 +70,86 @@ FE = ('gl', 'hu', 'as', 'ar', 'am')
 DS = VP = UNDEAD = ('pr', 'he')
 GH = MU = UNDEAD + ('tm',)
 
-combo_data = {
+class ComboData(object):
+    def __init__(self):
+        self.races = {
+            # field 3 of the tuple is class restrictions
+            'hu': ('Hu', 'Human', None),
+            'he': ('HE', 'High Elf', None),
+            'de': ('DE', 'Deep Elf', None),
+            'se': ('SE', 'Sludge Elf', None),
+            'dd': ('DD', 'Deep Dwarf', None),
+            'ho': ('HO', 'Hill Orc', None),
+            'mf': ('Mf', 'Merfolk', None),
+            'ha': ('Ha', 'Halfling', None),
+            'ko': ('Ko', 'Kobold', None),
+            'sp': ('Sp', 'Spriggan', None),
+            'na': ('Na', 'Naga', None),
+            'ce': ('Ce', 'Centaur', None),
+            'og': ('Og', 'Ogre', None),
+            'tr': ('Tr', 'Troll', None),
+            'mi': ('Mi', 'Minotaur', None),
+            'te': ('Te', 'Tengu', None),
+            'dr': ('Dr', 'Draconian', None),
+            'dg': ('Dg', 'Demigod', DG),
+            'ds': ('Ds', 'Demonspawn', DS),
+            'mu': ('Mu', 'Mummy', MU),
+            'gh': ('Gh', 'Ghoul', GH),
+            'vp': ('Vp', 'Vampire', VP),
+            'fe': ('Fe', 'Felid', FE),
+            'op': ('Op', 'Octopode', None),
+        }
 
-    'race': {
-        # field 3 of the tuple is class restrictions
-        'hu': ('Hu', 'Human', None),
-        'he': ('HE', 'High Elf', None),
-        'de': ('DE', 'Deep Elf', None),
-        'se': ('SE', 'Sludge Elf', None),
-        'dd': ('DD', 'Deep Dwarf', None),
-        'ho': ('HO', 'Hill Orc', None),
-        'mf': ('Mf', 'Merfolk', None),
-        'ha': ('Ha', 'Halfling', None),
-        'ko': ('Ko', 'Kobold', None),
-        'sp': ('Sp', 'Spriggan', None),
-        'na': ('Na', 'Naga', None),
-        'ce': ('Ce', 'Centaur', None),
-        'og': ('Og', 'Ogre', None),
-        'tr': ('Tr', 'Troll', None),
-        'mi': ('Mi', 'Minotaur', None),
-        'te': ('Te', 'Tengu', None),
-        'dr': ('Dr', 'Draconian', None),
-        'dg': ('Dg', 'Demigod', DG),
-        'ds': ('Ds', 'Demonspawn', DS),
-        'mu': ('Mu', 'Mummy', MU),
-        'gh': ('Gh', 'Ghoul', GH),
-        'vp': ('Vp', 'Vampire', VP),
-        'fe': ('Fe', 'Felid', FE),
-        'op': ('Op', 'Octopode', None),
-    },
+        self.classes = {
+            'fi': ('Fi', 'Fighter'),
+            'gl': ('Gl', 'Gladiator'),
+            'mo': ('Mo', 'Monk'),
+            'hu': ('Hu', 'Hunter'),
+            'as': ('As', 'Assassin'),
+            'ar': ('Ar', 'Artificer'),
+            'be': ('Be', 'Berserker'),
+            'ak': ('AK', 'Abyssal Knight'),
+            'ck': ('CK', 'Chaos Knight'),
+            'dk': ('DK', 'Death Knight'),
+            'pr': ('Pr', 'Priest'),
+            'he': ('He', 'Healer'),
+            'sk': ('Sk', 'Skald'),
+            'tm': ('Tm', 'Transmuter'),
+            'wr': ('Wr', 'Warper'),
+            'am': ('AM', 'Arcane Marksman'),
+            'en': ('En', 'Enchanter'),
+            'wz': ('Wz', 'Wizard'),
+            'cj': ('Cj', 'Conjurer'),
+            'su': ('Su', 'Summoner'),
+            'ne': ('Ne', 'Necromancer'),
+            'fe': ('FE', 'Fire Elementalist'),
+            'ie': ('IE', 'Ice Elementalist'),
+            'ae': ('AE', 'Air Elementalist'),
+            'ee': ('EE', 'Earth Elementalist'),
+            'vm': ('VM', 'Venom Mage'),
+        }
 
-    'class': {
-        'fi': ('Fi', 'Fighter'),
-        'gl': ('Gl', 'Gladiator'),
-        'mo': ('Mo', 'Monk'),
-        'hu': ('Hu', 'Hunter'),
-        'as': ('As', 'Assassin'),
-        'ar': ('Ar', 'Artificer'),
-        'be': ('Be', 'Berserker'),
-        'ak': ('AK', 'Abyssal Knight'),
-        'ck': ('CK', 'Chaos Knight'),
-        'dk': ('DK', 'Death Knight'),
-        'pr': ('Pr', 'Priest'),
-        'he': ('He', 'Healer'),
-        'sk': ('Sk', 'Skald'),
-        'tm': ('Tm', 'Transmuter'),
-        'wr': ('Wr', 'Warper'),
-        'am': ('AM', 'Arcane Marksman'),
-        'en': ('En', 'Enchanter'),
-        'wz': ('Wz', 'Wizard'),
-        'cj': ('Cj', 'Conjurer'),
-        'su': ('Su', 'Summoner'),
-        'ne': ('Ne', 'Necromancer'),
-        'fe': ('FE', 'Fire Elementalist'),
-        'ie': ('IE', 'Ice Elementalist'),
-        'ae': ('AE', 'Air Elementalist'),
-        'ee': ('EE', 'Earth Elementalist'),
-        'vm': ('VM', 'Venom Mage'),
-    },
-}
+combo_data = ComboData()
+
+def valid_character(race, cls):
+    race, cls = race.lower(), cls.lower()
+    short, long, restricted = combo_data.races[race]
+    return not cls in restricted if restricted else True
+
+VALID_CHARACTERS = tuple(
+    ((race + cls, race_data[0] + cls_data[0]) 
+        for race, race_data in combo_data.races.items()
+        for cls, cls_data in combo_data.classes.items()
+        if valid_character(race, cls)
+    )
+)
+
+if __name__ == '__main__':
+    races, classes = ['mi', 'mu', 'ho', 'fe'], ['dk', 'tm', 'be', 'gl']
+    tests = zip(races, classes)
+    for test in tests:
+        print test, valid_character(*test)
+    chars = sorted([can for s, can in VALID_CHARACTERS])
+    print len(chars)
+    print ', '.join(chars)
+
